ba7e8cf22953e02f1d02e74b8050e47d8d23fff2
ilangelov <ilian.petrov.angelov@gmail.com>
2025-02-24 15:24:34 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,47 +1,51 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2" # Change to your desired AWS region
 }
 
-resource "aws_eks_cluster" "devnet" {
-  name     = "go-ethereum-devnet"
-  role_arn = aws_iam_role.eks.arn
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
 
+resource "aws_eks_cluster" "example" {
+  name     = "my-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
   vpc_config {
-    subnet_ids = aws_subnet.devnet[*].id
+    subnet_ids = aws_subnet.subnet[*].id
   }
 }
 
-resource "kubernetes_deployment" "devnet" {
-  metadata {
-    name = "go-ethereum"
-    namespace = "default"
-  }
-
-  spec {
-    replicas = 1
-
-    selector {
-      match_labels = {
-        app = "go-ethereum"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          app = "go-ethereum"
+resource "aws_iam_role" "eks_cluster_role" {
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "eks.amazonaws.com"
         }
-      }
+        Effect = "Allow"
+        Sid    = ""
+      },
+    ]
+  })
+}
 
-      spec {
-        container {
-          image = "ipangelov/go-ethereum:deployed"
-          name  = "go-ethereum"
-          ports {
-            container_port = 8545
-          }
-        }
-      }
-    }
-  }
+resource "aws_subnet" "subnet" {
+  count = 2
+  cidr_block = "10.0.${count.index}.0/24"
+  vpc_id     = aws_vpc.vpc.id
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "my-cluster"
+  cluster_version = "1.21"
+  subnets         = aws_subnet.subnet[*].id
+  vpc_id          = aws_vpc.vpc.id
 }
